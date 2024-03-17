2cbab285933042a0aca97d168c76cbc576c24ea6
Daniel Iliev <47360939+NiX3r@users.noreply.github.com>
2024-03-17 20:41:22 +0100

    Update NixBot.java

--- NixBot/src/main/java/cz/nix3r/NixBot.java
+++ NixBot/src/main/java/cz/nix3r/NixBot.java
@@ -4,6 +4,8 @@ import cz.nix3r.enums.LogType;
 import cz.nix3r.utils.CommonUtils;
 import cz.nix3r.utils.LogSystem;
 
+import java.util.Scanner;
+
 public class NixBot {
 
     public static void main(String[] args) {
@@ -11,6 +13,11 @@ public class NixBot {
         LogSystem.log(LogType.INFO, "Bot started. Initializing ..");
         try{
             CommonUtils.setupBot();
+            Scanner scanner = new Scanner(System.in);
+            while(true){
+                String consoleCommand = scanner.nextLine();
+                System.out.println("Command from console: " + consoleCommand);
+            }
         }
         catch (Exception ex){
             LogSystem.log(LogType.FATAL_ERROR, "Bot can't be initialized or loaded. Error: " + ex.getMessage());
