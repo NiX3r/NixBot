4aced32d7b0f4527dd64124609179b670e875fab
Daniel Iliev <47360939+NiX3r@users.noreply.github.com>
2024-06-02 20:35:14 +0200

    final update

---
+++ NixBot/src/main/java/cz/iliev/managers/announcement_manager/AnnouncementManager.java
@@ -0,0 +1,221 @@
+package cz.iliev.managers.announcement_manager;
+
+import com.vdurmont.emoji.EmojiParser;
+import cz.iliev.interfaces.IManager;
+import cz.iliev.managers.announcement_manager.commands.AnnouncementCommand;
+import cz.iliev.managers.announcement_manager.instances.MessagesInstance;
+import cz.iliev.managers.announcement_manager.utils.AnnouncementManagerUtils;
+import cz.iliev.managers.announcement_manager.utils.FileUtils;
+import cz.iliev.managers.command_manager.CommandManager;
+import cz.iliev.managers.music_manager.instances.SongInstance;
+import cz.iliev.managers.role_manager.RoleManager;
+import cz.iliev.managers.role_manager.instances.RoleSetterInstance;
+import cz.iliev.utils.CommonUtils;
+import cz.iliev.utils.LogUtils;
+import org.javacord.api.entity.Icon;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.MessageFlag;
+import org.javacord.api.entity.message.MessageUpdater;
+import org.javacord.api.entity.message.component.ActionRow;
+import org.javacord.api.entity.message.component.Button;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.interaction.SlashCommandInteraction;
+
+import java.awt.*;
+import java.util.List;
+
+public class AnnouncementManager implements IManager {
+
+    private boolean ready;
+    private MessagesInstance messages;
+
+    private final String WELCOME_CHANNEL_ID = "611985124057284621";
+    public static final String NEWS_CHANNEL_ID = "1219218631632748655";
+    public static final String NIXBOT_CHANNEL_ID = "1058017127988211822";
+
+    public AnnouncementManager(){
+        setup();
+    }
+
+    @Override
+    public void setup() {
+        LogUtils.info("Load and start AnnouncementManager");
+        messages = FileUtils.loadMessages();
+        ready = true;
+        LogUtils.info("AnnouncementManager loaded and started. Ready to use");
+    }
+
+    @Override
+    public void kill() {
+        LogUtils.info("kill AnnouncementManager");
+        FileUtils.saveMessages(messages);
+        ready = false;
+        LogUtils.info("AnnouncementManager killed");
+    }
+
+    @Override
+    public boolean restart() {
+        kill();
+        setup();
+        return ready;
+    }
+
+    @Override
+    public void onCommand(SlashCommandInteraction interaction) {
+        switch (interaction.getCommandName()){
+            case "announcement":
+                new AnnouncementCommand().run(interaction);
+                break;
+        }
+    }
+
+    @Override
+    public void onConsoleCommand(Object data) {
+        String[] command = ((String)data).split(" ");
+
+        switch (command[0]){
+            case "restart": case "r":
+                sendRestart(command);
+                break;
+        }
+    }
+
+    @Override
+    public boolean isReady() {
+        return ready;
+    }
+
+    @Override
+    public String managerName() {
+        return "Announcement manager";
+    }
+
+    @Override
+    public String managerDescription() {
+        return "Manager for sending announcements\nFeatures:\n- Restart announcement\n- Exception embed\n- Current song playing\n- Role setter\n- Welcome message";
+    }
+
+    @Override
+    public String color() {
+        return "#c52ae0";
+    }
+
+    private void sendRestart(String[] command){
+
+        if(command.length != 3){
+            LogUtils.warning("Bad command usage. Usage: 'announcements restart <time to restart>'");
+            return;
+        }
+
+        CommonUtils.bot.getServers().forEach(server -> {
+            server.getTextChannelById(NEWS_CHANNEL_ID).ifPresent(textChannel -> {
+
+                EmbedBuilder builder = new EmbedBuilder()
+                        .setTitle("Restart bota")
+                        .setDescription("Dojde k automatickému restartu bota.")
+                        .addField("Restart za", command[2] + " minut")
+                        .setColor(Color.GREEN)
+                        .setThumbnail("https://pluspng.com/img-png/restart-png-restart-icon-1600.png")
+                        .setFooter("Console | Version: " + CommonUtils.VERSION);
+
+                textChannel.sendMessage(builder);
+
+            });
+        });
+    }
+    
+    public void sendException(Exception exception, boolean isFatal){
+        CommonUtils.bot.getServers().forEach(server -> {
+            if(!server.getIdAsString().equals(CommonUtils.NIX_CREW_ID)){
+                LogUtils.warning("Leaving server '" + server.getName() + "'");
+                server.leave().join();
+            }
+            else{
+                server.getTextChannelById(NIXBOT_CHANNEL_ID).ifPresent(channel -> {
+                    channel.sendMessage(AnnouncementManagerUtils.createExceptionEmbed(exception, isFatal));
+                });
+            }
+        });
+    }
+
+    public void sendCurrentSong(SongInstance song){
+        CommonUtils.bot.getServers().forEach(server -> {
+            server.getTextChannelById(CommandManager.CMD_CHANNEL_ID).ifPresent(channel -> {
+                channel.sendMessage(AnnouncementManagerUtils.createCurrentSongEmbed(song));
+            });
+        });
+    }
+
+    public void sendRoleSetter(List<RoleSetterInstance> roleSetter){
+        CommonUtils.bot.getServers().forEach(server -> {
+            server.getTextChannelById(RoleManager.ROLES_CHANNEL_ID).ifPresent(channel -> {
+                Message msg = null;
+                try{
+                    msg = channel.getMessageById(CommonUtils.settings.getRolesMessageId()).get();
+                }
+                catch (Exception ex){
+                    LogUtils.warning("Role message does not exists. Creating a new one");
+                }
+                if(msg == null){
+                    MessageBuilder builder = new MessageBuilder()
+                            .setEmbed(AnnouncementManagerUtils.createRoleSetterEmbed(roleSetter.size()));
+                    roleSetter.forEach(setter -> {
+                        builder.addComponents(
+                                ActionRow.of(Button.secondary("nix-role-" + setter.getRoleId(), setter.getComponentLabel(), EmojiParser.parseToUnicode(setter.getEmoji())))
+                        );
+                    });
+                    msg = builder.send(channel).join();
+                    CommonUtils.settings.setRolesMessageId(msg.getId());
+                    LogUtils.info("Role message crated and sent. Message ID saved");
+                }
+                else{
+                    MessageUpdater updater = msg.createUpdater();
+                    updater.removeAllComponents().removeAllEmbeds()
+                            .addEmbed(AnnouncementManagerUtils.createRoleSetterEmbed(roleSetter.size()));
+
+                    roleSetter.forEach(setter -> {
+                        updater.addComponents(
+                                ActionRow.of(Button.secondary("nix-role-" + setter.getRoleId(), setter.getComponentLabel(), EmojiParser.parseToUnicode(setter.getEmoji())))
+                        );
+                    });
+
+                    updater.applyChanges().join();
+                    LogUtils.info("Role message updated");
+                }
+            });
+        });
+    }
+
+    public void sendWelcome(String user, Icon avatar, String inviter){
+
+        CommonUtils.bot.getServers().forEach(server -> {
+            if(!server.getIdAsString().equals(CommonUtils.NIX_CREW_ID)){
+                CommonUtils.politeDisconnect(server);
+                return;
+            }
+            var embed = AnnouncementManagerUtils.createJoinEmbed(user, inviter, avatar, server);
+            server.getTextChannelById(WELCOME_CHANNEL_ID).ifPresent(channel -> {
+                channel.sendMessage(embed);
+            });
+        });
+    }
+
+    public void sendRestart(String minutes){
+        CommonUtils.bot.getServers().forEach(server -> {
+            if(!server.getIdAsString().equals(CommonUtils.NIX_CREW_ID)){
+                CommonUtils.politeDisconnect(server);
+                return;
+            }
+            var embed = AnnouncementManagerUtils.createRestartAnnouncementEmbed(minutes);
+            server.getTextChannelById(NEWS_CHANNEL_ID).ifPresent(channel -> {
+                channel.sendMessage(embed);
+            });
+        });
+    }
+
+    public MessagesInstance getMessages() {
+        return messages;
+    }
+}
---
+++ NixBot/src/main/java/cz/iliev/utils/CommonUtils.java
@@ -0,0 +1,144 @@
+package cz.iliev.utils;
+
+import cz.iliev.instances.SettingsInstance;
+import cz.iliev.managers.announcement_manager.AnnouncementManager;
+import cz.iliev.managers.ban_list_manager.BanListManager;
+import cz.iliev.managers.bot_activity_manager.BotActivityManager;
+import cz.iliev.managers.command_manager.CommandManager;
+import cz.iliev.managers.command_manager.utils.CommandManagerUtils;
+import cz.iliev.managers.console_command_manager.ConsoleCommandManager;
+import cz.iliev.managers.main_manager.MainManager;
+import cz.iliev.managers.music_manager.MusicManager;
+import cz.iliev.managers.role_manager.RoleManager;
+import cz.iliev.managers.statistics_manager.StatisticsManager;
+import cz.iliev.managers.stay_fit_manager.StayFitManager;
+import cz.iliev.managers.temporary_channel_manager.TemporaryChannelManager;
+import cz.iliev.managers.ticket_manager.TicketManager;
+import cz.iliev.managers.user_verification_manager.UserVerificationManager;
+import cz.iliev.threads.ShutdownThread;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.permission.PermissionType;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+
+import java.util.concurrent.TimeUnit;
+
+public class CommonUtils {
+
+    public static DiscordApi bot;
+    public static SettingsInstance settings;
+
+    public static final String VERSION = "3.0.0";
+    public static final long START_TIME = System.currentTimeMillis();
+    public static final String NIX_CREW_ID = "611985124023730185";
+
+    public static AnnouncementManager announcementManager;
+    public static BanListManager banListManager;
+    public static BotActivityManager botActivityManager;
+    public static CommandManager commandManager;
+    public static ConsoleCommandManager consoleCommandManager;
+    public static MainManager mainManager;
+    public static MusicManager musicManager;
+    public static RoleManager roleManager;
+    public static StatisticsManager statisticsManager;
+    public static StayFitManager stayFitManager;
+    public static TemporaryChannelManager temporaryChannelManager;
+    public static TicketManager ticketManager;
+    public static UserVerificationManager userVerificationManager;
+
+    public static void setupBot(){
+        LogUtils.info("Load settings from file");
+
+        FileUtils.createDefaultFolders();
+
+        settings = FileUtils.loadSettings();
+        if(settings == null){
+            LogUtils.fatalError("Can't load settings. Turning the bot off");
+            return;
+        }
+
+        LogUtils.info("Initialize and connect bot");
+        bot = new DiscordApiBuilder().setToken(settings.getBotToken()).setAllIntents().login().join();
+
+        LogUtils.info("Setup managers");
+        announcementManager = new AnnouncementManager();
+        banListManager = new BanListManager();
+        botActivityManager = new BotActivityManager();
+        commandManager = new CommandManager();
+        consoleCommandManager = new ConsoleCommandManager();
+        musicManager = new MusicManager();
+        roleManager = new RoleManager();
+        statisticsManager = new StatisticsManager();
+        stayFitManager = new StayFitManager();
+        temporaryChannelManager = new TemporaryChannelManager();
+        ticketManager = new TicketManager();
+        userVerificationManager = new UserVerificationManager();
+        mainManager = new MainManager();
+
+        LogUtils.info("Initializing and starting threads");
+        Runtime.getRuntime().addShutdownHook(new ShutdownThread());
+
+        // Refresh commands if needed
+        //CommandManagerUtils.deleteCommands();
+        //CommandManagerUtils.createCommands();
+
+        LogUtils.info("Bot successfully initialized and loaded. It took " + (System.currentTimeMillis() - START_TIME) + "ms");
+    }
+
+    public static void shutdownBot(){
+        announcementManager.kill();
+        banListManager.kill();
+        botActivityManager.kill();
+        commandManager.kill();
+        consoleCommandManager.kill();
+        mainManager.kill();
+        musicManager.kill();
+        roleManager.kill();
+        statisticsManager.kill();
+        stayFitManager.kill();
+        temporaryChannelManager.kill();
+        ticketManager.kill();
+        userVerificationManager.kill();
+        FileUtils.saveSettings(settings);
+    }
+
+    public static void throwException(Exception exception){throwException(exception, false, false);}
+    public static void throwException(Exception exception, boolean isFatal, boolean bypassDiscordMessage){
+
+        if(isFatal) LogUtils.fatalError(exception.toString());
+        else LogUtils.error(exception.toString());
+
+        if(!bypassDiscordMessage)
+            announcementManager.sendException(exception, isFatal);
+
+    }
+
+    public static void politeDisconnect(Server server){
+        server.getMembers().forEach(member -> {
+            if(server.hasPermission(member, PermissionType.ADMINISTRATOR)){
+                member.sendMessage("Unfortunately NixBot is private bot for private server. Bot is leaving your server now.");
+            }
+        });
+        server.leave().join();
+    }
+
+    public static boolean isUserAdmin(User user){
+        for(Server server : bot.getServers()){
+            if(!server.getIdAsString().equals(NIX_CREW_ID)){
+                politeDisconnect(server);
+                continue;
+            }
+            return server.hasPermission(user, PermissionType.ADMINISTRATOR);
+        }
+        return false;
+    }
+
+    public static String formatTimeToMinutes(long time){
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(time);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(time) % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+
+}
